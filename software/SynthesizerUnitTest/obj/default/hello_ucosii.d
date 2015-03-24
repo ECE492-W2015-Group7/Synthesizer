044e334eed19edead697b7bc3b448f2509fbc3d9
unknown <qsjiang@e3-11-30.ece.ualberta.ca>
2015-03-23 18:52:12 -0600

    phase increment seems to be working, but haven't validate the output of the synth

---
+++ software/SynthesizerUnitTest/obj/default/hello_ucosii.d
@@ -0,0 +1,69 @@
+obj/default/hello_ucosii.o: hello_ucosii.c \
+  ../SynthesizerUnitTest_bsp//HAL/inc/includes.h \
+  ../SynthesizerUnitTest_bsp//HAL/inc/os_cpu.h \
+  ../SynthesizerUnitTest_bsp//HAL/inc/sys/alt_irq.h \
+  ../SynthesizerUnitTest_bsp//HAL/inc/nios2.h \
+  ../SynthesizerUnitTest_bsp//HAL/inc/alt_types.h \
+  ../SynthesizerUnitTest_bsp/system.h ../SynthesizerUnitTest_bsp/linker.h \
+  ../SynthesizerUnitTest_bsp//UCOSII/inc/os_cfg.h \
+  ../SynthesizerUnitTest_bsp//HAL/inc/sys/alt_alarm.h \
+  ../SynthesizerUnitTest_bsp//HAL/inc/sys/alt_llist.h \
+  ../SynthesizerUnitTest_bsp//HAL/inc/priv/alt_alarm.h \
+  ../SynthesizerUnitTest_bsp//HAL/inc/alt_types.h \
+  ../SynthesizerUnitTest_bsp/system.h \
+  ../SynthesizerUnitTest_bsp//UCOSII/inc/ucos_ii.h \
+  ../SynthesizerUnitTest_bsp//UCOSII/inc/os_cfg.h \
+  ../SynthesizerUnitTest_bsp//HAL/inc/os_cpu.h \
+  ../SynthesizerUnitTest_bsp//drivers/inc/altera_avalon_jtag_uart_regs.h \
+  ../SynthesizerUnitTest_bsp//HAL/inc/io.h \
+  ../SynthesizerUnitTest_bsp//HAL/inc/alt_types.h \
+  ../SynthesizerUnitTest_bsp//drivers/inc/altera_avalon_fifo_util.h \
+  ../SynthesizerUnitTest_bsp//drivers/inc/altera_avalon_fifo_regs.h \
+  ../SynthesizerUnitTest_bsp//drivers/inc/altera_avalon_fifo_regs.h \
+  ../SynthesizerUnitTest_bsp//drivers/inc/altera_avalon_fifo.h
+
+../SynthesizerUnitTest_bsp//HAL/inc/includes.h:
+
+../SynthesizerUnitTest_bsp//HAL/inc/os_cpu.h:
+
+../SynthesizerUnitTest_bsp//HAL/inc/sys/alt_irq.h:
+
+../SynthesizerUnitTest_bsp//HAL/inc/nios2.h:
+
+../SynthesizerUnitTest_bsp//HAL/inc/alt_types.h:
+
+../SynthesizerUnitTest_bsp/system.h:
+
+../SynthesizerUnitTest_bsp/linker.h:
+
+../SynthesizerUnitTest_bsp//UCOSII/inc/os_cfg.h:
+
+../SynthesizerUnitTest_bsp//HAL/inc/sys/alt_alarm.h:
+
+../SynthesizerUnitTest_bsp//HAL/inc/sys/alt_llist.h:
+
+../SynthesizerUnitTest_bsp//HAL/inc/priv/alt_alarm.h:
+
+../SynthesizerUnitTest_bsp//HAL/inc/alt_types.h:
+
+../SynthesizerUnitTest_bsp/system.h:
+
+../SynthesizerUnitTest_bsp//UCOSII/inc/ucos_ii.h:
+
+../SynthesizerUnitTest_bsp//UCOSII/inc/os_cfg.h:
+
+../SynthesizerUnitTest_bsp//HAL/inc/os_cpu.h:
+
+../SynthesizerUnitTest_bsp//drivers/inc/altera_avalon_jtag_uart_regs.h:
+
+../SynthesizerUnitTest_bsp//HAL/inc/io.h:
+
+../SynthesizerUnitTest_bsp//HAL/inc/alt_types.h:
+
+../SynthesizerUnitTest_bsp//drivers/inc/altera_avalon_fifo_util.h:
+
+../SynthesizerUnitTest_bsp//drivers/inc/altera_avalon_fifo_regs.h:
+
+../SynthesizerUnitTest_bsp//drivers/inc/altera_avalon_fifo_regs.h:
+
+../SynthesizerUnitTest_bsp//drivers/inc/altera_avalon_fifo.h:
