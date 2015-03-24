d4457de1b4447bd450834b87f2b1202304180531
unknown <qsjiang@e3-11-30.ece.ualberta.ca>
2015-03-23 18:15:25 -0600

    able to read synth data from fifo, audio not working yet

---
+++ software/SynthUnitTest/obj/default/hello_ucosii.d
@@ -0,0 +1,67 @@
+obj/default/hello_ucosii.o: hello_ucosii.c \
+  ../SynthUnitTest_bsp//HAL/inc/includes.h \
+  ../SynthUnitTest_bsp//HAL/inc/os_cpu.h \
+  ../SynthUnitTest_bsp//HAL/inc/sys/alt_irq.h \
+  ../SynthUnitTest_bsp//HAL/inc/nios2.h \
+  ../SynthUnitTest_bsp//HAL/inc/alt_types.h ../SynthUnitTest_bsp/system.h \
+  ../SynthUnitTest_bsp/linker.h ../SynthUnitTest_bsp//UCOSII/inc/os_cfg.h \
+  ../SynthUnitTest_bsp//HAL/inc/sys/alt_alarm.h \
+  ../SynthUnitTest_bsp//HAL/inc/sys/alt_llist.h \
+  ../SynthUnitTest_bsp//HAL/inc/priv/alt_alarm.h \
+  ../SynthUnitTest_bsp//HAL/inc/alt_types.h ../SynthUnitTest_bsp/system.h \
+  ../SynthUnitTest_bsp//UCOSII/inc/ucos_ii.h \
+  ../SynthUnitTest_bsp//UCOSII/inc/os_cfg.h \
+  ../SynthUnitTest_bsp//HAL/inc/os_cpu.h \
+  ../SynthUnitTest_bsp//drivers/inc/altera_avalon_jtag_uart_regs.h \
+  ../SynthUnitTest_bsp//HAL/inc/io.h \
+  ../SynthUnitTest_bsp//HAL/inc/alt_types.h \
+  ../SynthUnitTest_bsp//drivers/inc/altera_avalon_fifo_util.h \
+  ../SynthUnitTest_bsp//drivers/inc/altera_avalon_fifo_regs.h \
+  ../SynthUnitTest_bsp//drivers/inc/altera_avalon_fifo_regs.h \
+  ../SynthUnitTest_bsp//drivers/inc/altera_avalon_fifo.h
+
+../SynthUnitTest_bsp//HAL/inc/includes.h:
+
+../SynthUnitTest_bsp//HAL/inc/os_cpu.h:
+
+../SynthUnitTest_bsp//HAL/inc/sys/alt_irq.h:
+
+../SynthUnitTest_bsp//HAL/inc/nios2.h:
+
+../SynthUnitTest_bsp//HAL/inc/alt_types.h:
+
+../SynthUnitTest_bsp/system.h:
+
+../SynthUnitTest_bsp/linker.h:
+
+../SynthUnitTest_bsp//UCOSII/inc/os_cfg.h:
+
+../SynthUnitTest_bsp//HAL/inc/sys/alt_alarm.h:
+
+../SynthUnitTest_bsp//HAL/inc/sys/alt_llist.h:
+
+../SynthUnitTest_bsp//HAL/inc/priv/alt_alarm.h:
+
+../SynthUnitTest_bsp//HAL/inc/alt_types.h:
+
+../SynthUnitTest_bsp/system.h:
+
+../SynthUnitTest_bsp//UCOSII/inc/ucos_ii.h:
+
+../SynthUnitTest_bsp//UCOSII/inc/os_cfg.h:
+
+../SynthUnitTest_bsp//HAL/inc/os_cpu.h:
+
+../SynthUnitTest_bsp//drivers/inc/altera_avalon_jtag_uart_regs.h:
+
+../SynthUnitTest_bsp//HAL/inc/io.h:
+
+../SynthUnitTest_bsp//HAL/inc/alt_types.h:
+
+../SynthUnitTest_bsp//drivers/inc/altera_avalon_fifo_util.h:
+
+../SynthUnitTest_bsp//drivers/inc/altera_avalon_fifo_regs.h:
+
+../SynthUnitTest_bsp//drivers/inc/altera_avalon_fifo_regs.h:
+
+../SynthUnitTest_bsp//drivers/inc/altera_avalon_fifo.h:
